bcb30f27f1ed862820df5a5c86f703a1fb1bab3b
Noloxe-Fred <noloxe.web@gmail.com>
2019-10-27 20:51:07 +0100

    Test question/réponse V1 ok

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,38 @@
 const { MessageEmbed } = require('discord.js');
 
+const createCharacter = message => {
+	message.channel.send('Voulez vous créer un personnage?');
+	message.channel.send('👍: Oui 👎: Non')
+		.then(async message => {
+			await message.react('👍');
+			await message.react('👎');
+			await message.react('✅');
+
+			message.awaitReactions(r => ['👍', '👎', '✅'].includes(r.emoji.name), { max: 3 })
+				.then(collected => {
+					const userChoice = collected;
+					console.log('REACTION', userChoice);
+
+					if (userChoice.find(choice => choice.emoji.name === '✅' && choice.count === 2)) {
+						if (userChoice.find(choice => choice.emoji.name === '👍')) {
+							return message.channel.send('Allons-y! Etape 1: blablabla');
+						}
+						if (userChoice.find(choice => choice.emoji.name === '👎')) {
+							return message.channel.send('Petit joueur')
+						}
+						message.channel.send('Vous n\'avez pas fait de choix')
+					}
+					message.channel.send('Vous n\'avez pas fait de choix')
+					createCharacter(message);
+				});
+		});
+}
+
 exports.run = (client, message, args) => {
-	message.channel.send('test')
+
+	createCharacter(message);
+	// .react(':thumbsdown:')
+
 };
 
 exports.help = {
