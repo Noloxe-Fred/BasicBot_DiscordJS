0a6526fe9efe6224d02ed4121d45db9920f67898
Frederic Moreau <frederic.moreau@kissthebride.fr>
2019-12-09 13:46:06 +0100

    Modif message bienvenue

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
 module.exports = (client, member) => {
 	const channel = client.channels.find(r => r.name === 'general'); // Comme il n'y a plus de paramètre msg, nous utilisons "client" pour trouver un channel
-	channel.send(`${member} vient de rejoindre le serveur`);
+	channel.send(`${member} a survécu`);
 };
