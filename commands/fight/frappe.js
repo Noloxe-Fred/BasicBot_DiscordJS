7576e3bc061ac76561ddfc2608c687b50151236b
Frederic Moreau <frederic.moreau@kissthebride.fr>
2019-12-18 14:07:19 +0100

    Add embed to xpSystem

--- commands/fight/frappe.js
+++ commands/fight/frappe.js
@@ -1,7 +1,7 @@
 const { addXp } = require('../../utils/xpSystem.js');
 
 exports.run = async (client, message, args) => {
-	message.channel.send('Vous avez tué le streumon, vous gagnez 8 XP!');
+	message.channel.send('Vous avez tué le streumon');
 	console.log('FRAPPE', addXp)
 	addXp(client, message, 8);
 };
--- utils/xpSystem.js
+++ utils/xpSystem.js
@@ -1,7 +1,10 @@
+const { MessageEmbed } = require('discord.js');
 const { XPMAX } = require('./constants.js');
 
 const addXp = async (client, message, xp) => {
 	const character = await client.getCharacterSheet(message.author);
+	const embed = new MessageEmbed()
+		.setTitle(`Vous avez gagné ${xp} XP`);
 
 	let newXp = character.main.xp + xp;
 	let newLevel = character.main.level;
@@ -10,11 +13,16 @@ const addXp = async (client, message, xp) => {
 		newLevel += 1;
 		newXp -= XPMAX;
 		character.main.xp = newXp;
-		message.channel.send('Vous avez gagné un niveau')
+		embed.setDescription('Vous avez gagné un niveau');
 	}
 
 	await client.updateCharacterSheetByUser(message.author, { main: { ...character.main, xp: newXp, level: newLevel } });
-	message.channel.send(`XP: ${newXp} || Niveau: ${newLevel}`);
+
+	embed
+		.addField('XP', `${newXp}/${XPMAX}`, true)
+		.addField('Niveau', `${newLevel}`, true);
+
+	message.channel.send(embed);
 };
 
 module.exports = {
